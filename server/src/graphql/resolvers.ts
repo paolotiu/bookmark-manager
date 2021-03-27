359f289b69ba416316781407088e9f3104ce9f54
Paolo Tiu <paolotiu17@gmail.com>
2021-03-27 22:25:08 +0800

    simple queries

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,6 +1,18 @@
+import { Bookmark } from '@entity/Bookmark';
+import { User } from '@entity/User';
+import { UserInputError } from 'apollo-server-errors';
 import { Resolvers } from './types';
 export const resolvers: Resolvers = {
     Query: {
         helloWorld: () => `Hello world`,
+        bookmark: async (_, { id }) => {
+            const bookmark = await Bookmark.findOne(id);
+            if (!bookmark) throw new UserInputError('No bookmark with that id');
+            return bookmark;
+        },
+    },
+    Mutation: {
+        signUp: (_, args) => User.create(args).save(),
+        createBookmark: (_, { data }) => Bookmark.create(data).save(),
     },
 };
--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,7 +1,31 @@
 import { gql } from 'apollo-server-core';
 
 export const typeDefs = gql`
+    type User {
+        id: Int!
+        email: String!
+        bookmarks: [Bookmark]!
+    }
+
+    type Bookmark {
+        id: Int!
+        title: String!
+        description: String
+    }
+
+    input CreateBookmarkInput {
+        title: String!
+        url: String!
+        description: String
+    }
+
     type Query {
         helloWorld: String!
+        bookmark(id: Int!): Bookmark!
+    }
+
+    type Mutation {
+        signUp(email: String!, name: String!): User!
+        createBookmark(data: CreateBookmarkInput!): Bookmark!
     }
 `;
