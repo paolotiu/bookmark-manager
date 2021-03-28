dc9174c04e65d9d0550bbb20241e544fa00481c0
Paolo Tiu <paolotiu17@gmail.com>
2021-03-28 16:44:37 +0800

    updated paths

--- server/src/startsServer.ts
+++ server/src/startsServer.ts
@@ -2,18 +2,19 @@ import { createConnection } from 'typeorm';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import 'reflect-metadata';
-import { typeDefs } from './graphql/typeDefs';
-import { resolvers } from './graphql/resolvers';
+import { typeDefs } from '@gql/typeDefs';
+import { resolvers } from '@gql/resolvers';
 export const startServer = async (): Promise<void> => {
     const app = express();
     const apolloServer = new ApolloServer({
         typeDefs,
         resolvers,
+        tracing: true,
     });
 
     apolloServer.applyMiddleware({ app, path: '/graphql' });
 
-    createConnection().then(() => {
+    createConnection().then(async () => {
         app.listen(4000, () => console.log(`Live on http://localhost:4000${apolloServer.graphqlPath}`));
     });
 };
