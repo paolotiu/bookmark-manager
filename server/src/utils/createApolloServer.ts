56337c18c47a202d11ba2dcb4aee6bf57b35c0d5
Paolo Tiu <paolotiu17@gmail.com>
2021-03-30 17:25:27 +0800

    put server and db to utils

--- server/src/utils/createApolloServer.ts
+++ server/src/utils/createApolloServer.ts
@@ -2,10 +2,9 @@ import { ApolloServer } from 'apollo-server-express';
 import { schema } from '@utils/schema';
 import { Express } from 'express';
 
-export const createApolloServer = async (app?: Express): Promise<ApolloServer> => {
+export const createApolloServer = (app?: Express): ApolloServer => {
     const apolloServer = new ApolloServer({
         schema,
-        tracing: true,
         context: ({ req, res }) => {
             return { req, res, userId: req.userId || undefined };
         },
