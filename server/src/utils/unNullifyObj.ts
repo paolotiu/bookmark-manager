f905f46b81c74edc6ff19e5ea3d599b1d42b8d16
Paolo Tiu <paolotiu17@gmail.com>
2021-04-01 14:13:35 +0800

    true unNullify

--- server/src/utils/unNullifyObj.ts
+++ server/src/utils/unNullifyObj.ts
@@ -1,5 +1,10 @@
 export const unNullifyObj = <T>(obj: T): { [key: string]: string | number | Date } => {
     const entries = Object.entries(obj);
-    const cleanedObj = Object.fromEntries(entries.filter((_, val) => val != null));
+    const cleanedObj = Object.fromEntries(
+        entries.filter(([, val]) => {
+            // False is val is null or undefined
+            return val != null;
+        }),
+    );
     return cleanedObj;
 };
