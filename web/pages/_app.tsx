80066703379dea4dfffe22bf4a60c817e675dade
Paolo Tiu <paolotiu17@gmail.com>
2021-04-04 14:27:53 +0800

    tailwind setup

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../lib/apolloClient';
 import { AppProps } from 'next/app';
+import 'styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     const apolloClient = useApollo(pageProps);
