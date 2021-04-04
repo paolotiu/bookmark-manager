6a79823da8281e5f7a4ea5f1260cc8df892d3281
Paolo Tiu <paolotiu17@gmail.com>
2021-04-04 17:08:11 +0800

    added typename to fields requested

--- web/graphql/auth/loginMutation.ts
+++ web/graphql/auth/loginMutation.ts
@@ -4,10 +4,12 @@ export const LOGIN_MUTATION = gql`
     mutation Login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
             ... on User {
+                __typename
                 id
             }
 
             ... on BaseError {
+                __typename
                 path
                 message
             }
