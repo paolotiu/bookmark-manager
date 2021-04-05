33ac3cfec2ce3fe15aa3dc3df8fe1e71ec4d50dd
Paolo Tiu <paolotiu17@gmail.com>
2021-04-05 15:02:38 +0800

    addlogin herok side

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -1,11 +1,17 @@
 import Login from '@components/Login';
 import React from 'react';
 
+import Test from '@assets/loginAssets/hero.svg';
+
 const login = () => {
     return (
-        <div className='h-screen md:flex md:justify-between '>
-            <Login className='w-full max-w-4xl lg:w-1/2 md:self-center md:pb-40 md:px-32 lg:pl-20 lg:pr-24 xl:pl-32 4xl:pl-72 4xl:pr-0' />
-            <div className='bg-black lg:w-1/2'></div>
+        <div className='h-screen md:flex md:justify-between'>
+            <Login className='self-center w-full max-w-4xl lg:w-1/2 md:px-32 lg:pl-20 lg:pr-24 xl:pl-32 4xl:pl-72 4xl:pr-0' />
+            <div className='relative hidden w-full bg-white lg:block lg:w-1/2'>
+                <div className='absolute right-0 flex justify-end w-full h-screen '>
+                    <Test className='max-h-full bg-primary ' />
+                </div>
+            </div>
         </div>
     );
 };
