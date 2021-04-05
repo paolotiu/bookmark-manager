6fe13ee0cdb28fb568a179fd82851f601bcdc471
Paolo Tiu <paolotiu17@gmail.com>
2021-04-05 15:59:04 +0800

    added hidden prop

--- web/components/General/ErrorMessage.tsx
+++ web/components/General/ErrorMessage.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 interface Props extends React.ComponentPropsWithoutRef<'span'> {
     text?: string;
+    hidden?: boolean;
 }
 
-const ErrorMessage = ({ text }: Props) => {
-    if (!text) return <> </>;
+const ErrorMessage = ({ text, hidden }: Props) => {
+    if (!text || hidden) return <> </>;
     return <span className='text-sm font-medium text-red-500 md:text-base'>{text}</span>;
 };
 
