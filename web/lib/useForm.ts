071f101047c72eb5a2dbbb65557c96d46a1111c5
Paolo Tiu <paolotiu17@gmail.com>
2021-04-05 15:58:57 +0800

    added error checking

--- web/lib/useForm.ts
+++ web/lib/useForm.ts
@@ -1,14 +1,63 @@
 import React, { useEffect, useState } from 'react';
+import { ObjectSchema } from 'yup';
 
-export const useForm = <T extends Record<string, unknown>>(initialState: T) => {
+interface ValidationError {
+    errors: string[] | null;
+    message: string | null;
+}
+
+const nulledValidationError = {
+    errors: null,
+    message: null,
+};
+
+export const useForm = <T extends Record<string, unknown>>(initialState: T, schema?: ObjectSchema<any>) => {
+    const emptyObject = <T extends Record<string, unknown>>(obj: T): { [K in keyof T]: '' } => {
+        return Object.fromEntries(Object.entries(obj).map(([key]) => [key, ''])) as any;
+    };
+
+    const nullifyObject = <T extends Record<string, unknown>>(obj: T): { [K in keyof T]: ValidationError } => {
+        return Object.fromEntries(Object.entries(obj).map(([key]) => [key, nulledValidationError])) as any;
+    };
+
     const [inputs, setInputs] = useState(initialState);
     const [isEmpty, setIsEmpty] = useState(true);
+    const [errors, setErrors] = useState<{ [K in keyof T]: ValidationError }>(nullifyObject(initialState));
+    const [isError, setIsError] = useState(true);
+
+    const checkIfEmpty = <T extends Record<string, unknown>>(obj: T) => {
+        return Object.values(obj).every((val) => val === '');
+    };
 
     useEffect(() => {
-        setIsEmpty(Object.values(inputs).every((val) => val === ''));
-    }, [inputs]);
+        // Runs whenever
+        const run = async () => {
+            if (schema) {
+                const res = await schema?.validate(inputs, { abortEarly: false }).catch((e) => e);
+                if (res.name === 'ValidationError') {
+                    // Validation error occurs
+                    setErrors(() =>
+                        res.inner.reduce(
+                            (acc: any, curr: any) => ({
+                                ...acc,
+                                [curr.path]: { errors: curr.errors, message: curr.message },
+                            }),
+                            nullifyObject(inputs),
+                        ),
+                    );
+                    setIsError(true);
+                } else {
+                    setErrors((prev) => nullifyObject(prev));
+                    setIsError(false);
+                }
+            }
+        };
+        setIsEmpty(checkIfEmpty(inputs));
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        run();
+    }, [inputs, schema]);
+
+    const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
         const { value, name } = e.target;
 
         setInputs((prev) => ({ ...prev, [name]: value }));
@@ -19,8 +68,8 @@ export const useForm = <T extends Record<string, unknown>>(initialState: T) => {
     };
 
     const clearForm = () => {
-        setInputs(Object.fromEntries(Object.entries(inputs).map(([key]) => [key, ''])) as T);
+        setInputs(emptyObject(inputs) as T);
     };
 
-    return { handleChange, resetForm, clearForm, inputs, isEmpty };
+    return { handleChange, resetForm, clearForm, inputs, isEmpty, errors, isError };
 };
