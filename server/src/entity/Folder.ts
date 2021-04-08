6a94fe88299c88ed5bbd65a191b95e7bcc155172
Paolo Tiu <paolotiu17@gmail.com>
2021-04-08 17:04:44 +0800

    get folder children implemented

--- server/src/entity/Folder.ts
+++ server/src/entity/Folder.ts
@@ -2,7 +2,6 @@ import {
     BaseEntity,
     Column,
     CreateDateColumn,
-    DeleteDateColumn,
     Entity,
     JoinColumn,
     ManyToOne,
@@ -16,27 +15,31 @@ import { Bookmark } from '@entity/Bookmark';
 @Unique(['name', 'userId', 'depth'])
 @Entity()
 export class Folder extends BaseEntity {
+    children: Folder[];
+
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column()
     name: string;
 
-    @Column({ type: 'int', default: 0 })
-    depth: number;
-
-    @Column({ type: 'int', nullable: true })
-    parentId: number;
+    // @Column({ type: 'int', nullable: true })
+    // parentId: number;
 
     @Column('int')
     userId: number;
 
-    @ManyToOne(() => Folder, (folder) => folder.parentId)
-    @JoinColumn({ name: 'parentId' })
-    parent: Folder;
+    @Column('ltree', { nullable: true })
+    path: string;
 
-    @OneToMany(() => Folder, (folder) => folder.parent)
-    children: Folder[];
+    // @TreeParent()
+    // parent: Folder;
+
+    // @TreeChildren({ cascade: true })
+    // children: Folder[];
+
+    @Column('int', { default: 0 })
+    depth: number;
 
     @ManyToOne(() => User, (user) => user.folders)
     @JoinColumn({ name: 'userId' })
@@ -48,6 +51,37 @@ export class Folder extends BaseEntity {
     @CreateDateColumn()
     createdDate: Date;
 
-    @DeleteDateColumn()
-    deletedDate: Date;
+    @Column('boolean', { default: false })
+    deleted: boolean;
+
+    private arrayToTree(arr: Folder[]): Folder {
+        const map: Record<string, Folder> = {};
+        for (let i = 0; i < arr.length; i++) {
+            map[arr[i].id] = arr[i];
+            arr[i].children = [];
+        }
+        arr.forEach((folder, i) => {
+            const regex = /\w+\./g;
+            const regexMatches = [...folder.path.matchAll(regex)];
+
+            // Skip root node
+            if (i) {
+                // Gets the parent id form the path
+                // 1.2.3 => 2
+                const parentId = regexMatches[regexMatches.length - 1][0].slice(0, -1);
+                map[parentId].children.push(folder);
+            }
+        });
+
+        return arr[0];
+    }
+
+    getDescendants(): Promise<Folder[]> {
+        return Folder.createQueryBuilder().select('*').where('path <@ :path', { path: this.path }).execute();
+    }
+
+    async getDescendantsTree(): Promise<Folder> {
+        const childrenArr = await this.getDescendants();
+        return this.arrayToTree(childrenArr);
+    }
 }
