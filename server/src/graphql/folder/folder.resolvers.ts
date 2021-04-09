bcb9fcd38c1ddef590c4bc6c04706a09598f2843
Paolo Tiu <paolotiu17@gmail.com>
2021-04-09 18:04:28 +0800

    added better error messages

--- server/src/graphql/folder/folder.resolvers.ts
+++ server/src/graphql/folder/folder.resolvers.ts
@@ -1,6 +1,11 @@
 import { Bookmark } from '@entity/Bookmark';
 import { Folder } from '@entity/Folder';
-import { isBaseError, unauthorizedError } from '@gql/shared/errorMessages';
+import {
+    createEntityIdNotFoundError,
+    createUnexpectedError,
+    isBaseError,
+    unauthorizedError,
+} from '@gql/shared/errorMessages';
 import { Resolvers } from '@gql/types';
 
 export const resolvers: Resolvers = {
@@ -21,13 +26,7 @@ export const resolvers: Resolvers = {
     Query: {
         folder: async (_, { id }, { userId }) => {
             if (!userId) return unauthorizedError('folder');
-            //TODO: ADD USER CHECK
-            return (
-                (await Folder.findOne(id)) || {
-                    path: 'folder',
-                    message: 'No folder with that id',
-                }
-            );
+            return (await Folder.findOne(id)) || createEntityIdNotFoundError('folder', 'folder');
         },
         q: async () => {
             const parent = await Folder.findOne(2);
@@ -36,48 +35,57 @@ export const resolvers: Resolvers = {
         },
     },
     Mutation: {
-        createFolder: async (_, { data: { name, parentId } }) => {
-            //TODO: ADD USER CHECK
+        createFolder: async (_, { data: { name, parentId } }, { userId }) => {
+            if (!userId) return unauthorizedError('createFolder');
 
-            const folder = Folder.create({ name, userId: 1 });
+            const folder = Folder.create({ name, userId });
 
             // default depth is 0 meaning no parent folder
             let depth = 0;
+            // Initialize path
             let path = '';
             if (parentId) {
                 // Check if parent id exists
                 const parent = await Folder.findOne(parentId);
                 if (parent) {
                     depth = parent.depth + 1;
+                    // Parent path + '.'
+                    // We'll insert the folder id later
                     path = parent.path + '.';
                 }
             }
             folder.depth = depth;
+            // save the folder without the path to generate an id
             const prePathFolder = await folder.save();
+
+            // Append the id to the path
             prePathFolder.path = path + prePathFolder.id;
+
             return prePathFolder.save();
         },
         moveFolder: async (_, { folderId, targetFolderId }, { userId }) => {
             if (!userId) return unauthorizedError('moveFolder');
+
+            // Get both target and source folder
             const foldersArr = await Folder.findByIds([folderId, targetFolderId], { where: { userId } });
-            if (foldersArr.length < 2) {
-                return {
-                    path: 'moveFolder',
-                    message: 'No folder with that id',
-                };
-            }
+
+            // Check if folders exists
+            if (foldersArr.length < 2) return createEntityIdNotFoundError('moveFolder', 'folder');
+
+            // Map folders by id
             const folders = foldersArr.reduce<{ [key: string]: Folder }>(
                 (prev, curr) => ({ ...prev, [curr.id]: curr }),
                 {},
             );
+
+            // Move folder
             const res = await folders[folderId].move(folders[targetFolderId].path);
+
+            // Get updated folder from query
             const updatedFolder = res[0].find((f) => f.id === folderId);
 
-            if (!updatedFolder)
-                return {
-                    path: 'moveFolder',
-                    message: 'An error has occured',
-                };
+            // This should not happen!!!
+            if (!updatedFolder) return createUnexpectedError('moveFolder');
 
             // Update folder path
             folders[folderId].path = updatedFolder.path;
@@ -88,32 +96,28 @@ export const resolvers: Resolvers = {
         updateFolderName: async (_, { name, id }, { userId }) => {
             if (!userId) return unauthorizedError('updateFolderName');
 
+            // Get folder
             const folder = await Folder.findOne(id, { where: { userId: userId } });
 
             // No folder found
-            if (!folder)
-                return {
-                    path: 'updateFolderName',
-                    message: 'No folder with that id',
-                };
+            if (!folder) return createEntityIdNotFoundError('updateFolderName', 'folder');
 
-            // Folder found => Change folder name
+            // Change folder name
             folder.name = name;
 
+            // Save then return
             return folder.save();
         },
         softDeleteFolder: async (_, { id }, { userId }) => {
             if (!userId) return unauthorizedError('deleteFolder');
 
-            // TODO: FIGURE OUT DELETION!!!!!
+            // Get folder
             const folder = await Folder.findOne(id, { where: { userId } });
-            // No folder found
-            if (!folder)
-                return {
-                    path: 'deleteFolder',
-                    message: 'No folder with that id',
-                };
 
+            // No folder found
+            if (!folder) return createEntityIdNotFoundError('softDeleteFolder', 'folder');
+
+            // Find find descendant folders
             await Folder.createQueryBuilder()
                 .update()
                 .set({
@@ -126,14 +130,10 @@ export const resolvers: Resolvers = {
         },
         recoverFolder: async (_, { id }, { userId }) => {
             if (!userId) return unauthorizedError('recoverFolder');
-            const folder = await Folder.findOne(id, { where: { userId }, withDeleted: true, relations: ['parent'] });
+            const folder = await Folder.update({ id, userId }, { deleted: false });
 
-            if (!folder)
-                return {
-                    path: 'recoverFolder',
-                    message: 'No folder with that id',
-                };
-            return folder;
+            if (folder.raw.length < 1) return createEntityIdNotFoundError('recoverFolder', 'folder');
+            return folder.raw[0];
         },
     },
 };
