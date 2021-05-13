a41fe59d29597f01416bd522f0eb0e017a924988
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-05-14 01:00:51 +0800

    added sizing

--- web/components/General/ErrorMessage.tsx
+++ web/components/General/ErrorMessage.tsx
@@ -3,11 +3,18 @@ import React from 'react';
 interface Props extends React.ComponentPropsWithoutRef<'span'> {
     text?: string;
     hidden?: boolean;
+    size?: 'small' | 'base';
 }
 
-const ErrorMessage = ({ text, hidden }: Props) => {
+const ErrorMessage = ({ text, hidden, size }: Props) => {
     if (!text || hidden) return <> </>;
-    return <span className='text-sm font-medium text-red-500 md:text-base'>{text}</span>;
+    return (
+        <span
+            className={`text-sm font-medium text-red-500 md:text-base ${size === 'small' ? `text-xs md:text-sm` : ``}`}
+        >
+            {text}
+        </span>
+    );
 };
 
 export default ErrorMessage;
