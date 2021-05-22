fb19d0081576aacf532bde54bbedb3dfa135e30b
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-05-22 21:49:48 +0800

    added redirect

--- web/components/Login.tsx
+++ web/components/Login.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Google from '@assets/google.svg';
-import { Divider } from 'tiu-ui';
 import PrimaryButton from '@components/General/PrimaryButton';
 import Input from '@components/General/Input';
 import { useForm } from '@lib/useForm';
@@ -9,6 +8,7 @@ import Link from 'next/link';
 import ErrorMessage from './General/ErrorMessage';
 import { checkObjEqual } from '@lib/checkObjEqual';
 import * as yup from 'yup';
+import { useRouter } from 'next/dist/client/router';
 
 // Inital form state
 const initialState = {
@@ -26,6 +26,7 @@ const Login = ({ className, ...p }: React.ComponentPropsWithoutRef<'div'>) => {
     const [lastInputs, setLastInputs] = useState(initialState);
     const [error, setError] = useState('');
     const [isShowError, setIsShowError] = useState(false);
+    const router = useRouter();
 
     const [loginMutation] = useLoginMutation();
 
@@ -46,31 +47,32 @@ const Login = ({ className, ...p }: React.ComponentPropsWithoutRef<'div'>) => {
         } else {
             setError('');
             // Do stuff
+            router.push('/home/all');
         }
     };
 
     return (
         <div className={`p-8 ${className || ''}`} {...p}>
-            <div className='py-4'>
-                <h1 className='py-4 text-4xl font-bold md:text-5xl'>Login</h1>
-                <p className='text-sm text-gray-500 md:text-lg'>Start organizing your bookmarks!</p>
+            <div className="py-4">
+                <h1 className="py-4 text-4xl font-bold md:text-5xl">Login</h1>
+                <p className="text-sm text-gray-500 md:text-lg">Start organizing your bookmarks!</p>
             </div>
-            <div className='pt-5'>
-                <button className='flex justify-center w-full px-4 py-2 border border-gray-300 rounded-full md:py-3 no-outline '>
-                    <Google className='h-5' />
-                    <span className='px-4'>Sign in with Google</span>
+            <div className="pt-5">
+                <button className="flex justify-center w-full px-4 py-2 border border-gray-300 rounded-full md:py-3 no-outline ">
+                    <Google className="h-5" />
+                    <span className="px-4">Sign in with Google</span>
                 </button>
-                <div className='text-xs font-medium py-7'>
-                    <Divider text='or Sign in with Email' lineColor='rgba(0,0,0,.2)' textColor='rgba(0,0,0,.4)' />
+                <div className="text-xs font-medium py-7">
+                    {/* <Divider text='or Sign in with Email' lineColor='rgba(0,0,0,.2)' textColor='rgba(0,0,0,.4)' /> */}
                 </div>
-                <form onSubmit={handleSubmit} className='grid gap-6'>
-                    <div className='grid gap-8'>
-                        <div className='grid gap-3'>
+                <form onSubmit={handleSubmit} className="grid gap-6">
+                    <div className="grid gap-8">
+                        <div className="grid gap-3">
                             <Input
-                                label='Email'
-                                type='email'
-                                name='email'
-                                placeholder='mail@example.com'
+                                label="Email"
+                                type="email"
+                                name="email"
+                                placeholder="mail@example.com"
                                 onChange={handleChange}
                                 value={inputs.email}
                             />
@@ -78,36 +80,36 @@ const Login = ({ className, ...p }: React.ComponentPropsWithoutRef<'div'>) => {
                             <ErrorMessage text={errors.email.message || ''} hidden={!isShowError} />
                         </div>
                         <Input
-                            label='Password'
-                            type='password'
-                            name='password'
-                            placeholder='Min. of 8 characters'
+                            label="Password"
+                            type="password"
+                            name="password"
+                            placeholder="Min. of 8 characters"
                             onChange={handleChange}
                             value={inputs.password}
                         />
                     </div>
 
                     <ErrorMessage text={error} />
-                    <Link href='/'>
-                        <a className='text-sm font-medium transition-colors duration-200 justify-self-end hover:underline text-primary hover:text-primary-dark md:text-base hover:cursor-pointer'>
+                    <Link href="/">
+                        <a className="text-sm font-medium transition-colors duration-200 justify-self-end hover:underline text-primary hover:text-primary-dark md:text-base hover:cursor-pointer">
                             Forgot password?
                         </a>
                     </Link>
-                    <div className='relative'>
+                    <div className="relative">
                         <div
                             className={`absolute z-10 w-full h-full ${isShowError ? 'hidden' : ''}`}
                             onClick={() => setIsShowError(true)}
                         ></div>
                         <PrimaryButton
                             disabled={isEmpty || isError}
-                            text='Login'
-                            className='w-full transition-all duration-200 transform hover:bg-primary-dark disabled:opacity-50 '
+                            text="Login"
+                            className="w-full transition-all duration-200 transform hover:bg-primary-dark disabled:opacity-50 "
                         />
                     </div>
-                    <span className='text-sm md:text-base'>
+                    <span className="text-sm md:text-base">
                         Don&apos;t have an account?{' '}
-                        <Link href='/register'>
-                            <a className='font-medium transition-colors duration-200 text-primary hover:underline hover:cursor-pointer hover:text-primary-dark'>
+                        <Link href="/register">
+                            <a className="font-medium transition-colors duration-200 text-primary hover:underline hover:cursor-pointer hover:text-primary-dark">
                                 Register
                             </a>
                         </Link>
