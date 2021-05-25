73cc02f9065c21b54d7f8b46ae71ac3eef90610a
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-05-25 18:48:48 +0800

    responsiveness

--- web/components/Settings/SettingsLayout.tsx
+++ web/components/Settings/SettingsLayout.tsx
@@ -13,7 +13,11 @@ const NavItem = ({ href, label }: NavItemProps) => {
     const isActive = route === href;
     return (
         <Link href={href}>
-            <a className={`p-2 rounded-sm hover:bg-white ${isActive && 'bg-white'}`}>
+            <a
+                className={`md:p-2 px-4 py-2 rounded-sm w-1/3 md:block text-center hover:bg-white ${
+                    isActive && 'bg-white'
+                }`}
+            >
                 <span className={`text-sm ${isActive && 'font-bold text-primary'}`}>{label}</span>
             </a>
         </Link>
@@ -28,9 +32,9 @@ const SettingsLayout = ({ children }: Props) => {
     return (
         <>
             <div className="h-full bg-altBg">
-                <div className="fixed z-10 flex items-center justify-center w-full bg-white border-b shadow-sm h-11">
+                <div className="fixed z-10 flex items-center justify-center w-full px-8 bg-white border-b shadow-sm h-11">
                     {/* <div className="flex items-center justify-between w-full max-w-[900px] px-4 py-4 text-xl max-w-screen "> */}
-                    <span className="w-[250px] pl-8">Settings</span>
+                    <span className="w-[250px] md:pl-8">Settings</span>
                     <div className="w-3/4 md:max-w-[704px]">
                         <Link href="/home/all">
                             <a className="flex items-center float-right py-2">
@@ -41,7 +45,7 @@ const SettingsLayout = ({ children }: Props) => {
                     {/* </div> */}
                 </div>
                 <div className="flex flex-col items-stretch min-h-screen md:flex-row md:justify-center">
-                    <nav className="flex justify-between px-8 pt-16 pb-4 md:justify-start md:w-64 md:p-8 md:flex-col md:pt-28 bg-altBg">
+                    <nav className="flex justify-between pt-16 pb-4 md:justify-start md:w-64 md:p-8 md:flex-col md:pt-28 bg-altBg">
                         <NavItem href="/settings/account" label="Account" />
                         <NavItem href="/settings/import" label="Import" />
                         <NavItem href="/settings/integrations" label="Integrations" />
