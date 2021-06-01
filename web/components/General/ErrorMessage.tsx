dc3a5b0052ed65d486159c927aad6a4a355a5572
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-06-01 14:59:22 +0800

    clsx convertion

--- web/components/General/ErrorMessage.tsx
+++ web/components/General/ErrorMessage.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 
 interface Props extends React.ComponentPropsWithoutRef<'span'> {
@@ -10,9 +11,13 @@ const ErrorMessage = ({ text, hidden, size, className }: Props) => {
     if (!text || hidden) return <> </>;
     return (
         <span
-            className={`text-sm font-medium text-red-500 md:text-base ${
-                size === 'small' ? `text-xs md:text-sm` : ``
-            } ${className}`}
+            className={clsx(
+                'text-sm font-medium text-red-500 md:text-base',
+                {
+                    'text-xs md:text-sm': size === 'small',
+                },
+                className,
+            )}
         >
             {text}
         </span>
